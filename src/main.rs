031bcbd05905bf73c6c07017b8240c57a7dfe5de
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2018-02-22 21:48:55 +0900

    guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;  // ioライブラリ読み込み
+
+fn main() {   // main がエントリーポイント
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // 変数束縛
+    // mut でミュータブル（可変）
+    let mut guess = String::new();  // 空文字列を束縛する
+
+    io::stdin()                          // 標準入力のハンドル
+        .read_line(&mut guess)           // メソッド呼び出し
+        .expect("Failed to read line");  // io::Result.expect()
+                                         // 呼ばないとwarning
+
+    println!("You guessed: {}", guess);  // {} がプレースホルダ
 }
