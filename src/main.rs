3f97e80473624083aa7c02312ec0de958ca0aaaf
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2018-02-27 21:40:38 +0900

    無限ループ

--- src/main.rs
+++ src/main.rs
@@ -12,30 +12,32 @@ fn main() {   // main がエントリーポイント
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // 変数束縛
-    // mut でミュータブル（可変）
-    // 型は型推論で決まってる
-    let mut guess = String::new();  // 空文字列を束縛する
+        // 変数束縛
+        // mut でミュータブル（可変）
+        // 型は型推論で決まってる
+        let mut guess = String::new();  // 空文字列を束縛する
 
-    io::stdin()                          // 標準入力のハンドル
-        .read_line(&mut guess)           // メソッド呼び出し
-        .expect("Failed to read line");  // io::Result.expect()
-                                         // 呼ばないとwarning
+        io::stdin()                          // 標準入力のハンドル
+            .read_line(&mut guess)           // メソッド呼び出し
+            .expect("Failed to read line");  // io::Result.expect()
+        // 呼ばないとwarning
 
-    // 変数を再定義 シャドーイング
-    // 変数の型を変えるため
-    let guess: u32 = guess.trim()  // 改行とかを削除
-        .parse()                   // 文字列を数値にする
-        .expect("Please type a number!");
+        // 変数を再定義 シャドーイング
+        // 変数の型を変えるため
+        let guess: u32 = guess.trim()  // 改行とかを削除
+            .parse()                   // 文字列を数値にする
+            .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);  // {} がプレースホルダ
+        println!("You guessed: {}", guess);  // {} がプレースホルダ
 
-    // 条件分岐
-    match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal   => println!("You win!"),
+        // 条件分岐
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => println!("You win!"),
+        }
     }
 }
