5ee3eb074ace220aa1b7d70d36529a1cfdd27de4
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2018-02-27 22:36:25 +0900

    処理をエミュレート

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
 // 構造体
 struct Philosopher {
     name: String,
@@ -16,6 +19,10 @@ impl Philosopher {
     }
 
     fn eat(&self) {
+        println!("{} is eating.", self.name);
+
+        thread::sleep(Duration::from_millis(1000));
+
         println!("{} is done eating.", self.name);
     }
 }
